c4a0c1b89836f59344bf2eeed53b98820d6d066e
alissonoliveira1 <alissonoliveira201339@gmail.com>
2023-10-18 13:30:46 -0300

    anima

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function b1(){
+  document.getElementById('volta').value = "Bahia"
+  document.getElementById('pop').style.display = 'none'
+}
+function b2(){
+  document.getElementById('volta').value = "Rio de Janeiro"
+  document.getElementById('pop').style.display = 'none'
+}
+function b3(){
+  document.getElementById('volta').value = "São Paulo"
+  document.getElementById('pop').style.display = 'none'
+}
+
+
 let hora = 24
 let segundos = 59
 let minutos = 59
@@ -93,6 +107,189 @@ const fundo = document.querySelector('.chh')
         document.querySelector('.icon-night').style.color = 'rgb(102, 102, 102)'
     }
 }
+
+
+
+
+
+
+
+const input = document.getElementById('ida');
+
+// Adiciona um evento de clique ao documento
+document.addEventListener('click', function (event) {
+  // Verifica se o alvo do clique não é o input
+  
+  if (event.target !== input) {
+    // Muda a cor de fundo do input
+    input.style.backgroundColor = 'white';
+
+      
+    
+  }
+});
+ 
+input.onclick = function(){
+    input.style.backgroundColor = '#bab490';
+   
+}
+
+const novoinput = document.getElementById('volta');
+document.addEventListener('click', function (event){
+    if(event.target !== novoinput){
+        novoinput.style.backgroundColor = 'white';
+    }
+});
+document.getElementById('volta').onclick = function(){
+    novoinput.style.backgroundColor = '#bab490'; 
+    document.getElementById('pop').style.display = 'flex'
+}
+
+
+
+
+
+ var kirin = document.querySelector('#korra')
+ document.querySelector('.tipos').onclick = function (){
+  if (kirin.checked){
+    document.querySelector('.tipos').style.backgroundColor = '#bab490'
+    document.querySelector('.paii').style.display = 'block'
+  }else{
+    document.querySelector('.tipos').style.backgroundColor = 'white'
+    document.querySelector('.paii').style.display = 'none'
+  }
+  
+ }
+
+ 
+
+
+var kiri = document.querySelector('#check-pass')
+document.querySelector('.pass').onclick = function (){
+ if (kiri.checked){
+   document.querySelector('.pass').style.backgroundColor = '#bab490'
+   document.querySelector('.odin').style.display = 'block'
+  
+ }else{
+   document.querySelector('.pass').style.backgroundColor = 'white'
+   document.querySelector('.odin').style.display = 'none'
+ }
+}
+
+
+
+var c1 = 0;
+var d1 = 1;
+var adulto = document.getElementById('adultos')
+function mais() {
+  document.getElementById('adultos').innerHTML = ++d1;
+  document.getElementById('a1').innerHTML = d1 + " Adulto(s)";
+  
+ if(d1 >= 10){
+  document.getElementById('adultos').innerHTML = 1;
+  document.getElementById('a1').innerHTML = 1 + " Adulto(s)";
+  d1 = 1
+  alert("O número máximo de passageiros deve ser de 9 pessoas.")
+  
+ }
+  var soma = d1 + c1
+  if( soma >=10){
+    alert('O número máximo de passageiros deve ser de 9 pessoas.')
+    document.getElementById('adultos').innerHTML = 0;
+  d1 = 0
+    c1 = 0
+    
+    document.getElementById('criancas').innerHTML = 0;
+  }
+}
+function menos() {
+  document.getElementById('adultos').innerHTML = --d1;
+  document.getElementById('a1').innerHTML = d1 + " Adulto(s)";
+  var atual = document.getElementById("adultos").value;
+  if(d1 < 1) { //evita números negativos
+    d1 = 1
+    document.getElementById('adultos').innerHTML = 0;
+  }
+}
+
+var crianca = document.getElementById('criancas')
+function mais1() {
+  document.getElementById('criancas').innerHTML = ++c1;
+  document.getElementById('c1').innerHTML =  c1 + " Criança(s)";
+  
+ if(c1 >= 10){
+  document.getElementById('criancas').innerHTML = 0;
+  c1 = 0
+  
+  alert("O número máximo de passageiros deve ser de 9 pessoas.")
+ }
+ if(c1 >= 1){
+  document.getElementById('c1').style.display = 'block'
+  document.getElementById('e1').style.display = 'block'
+ }
+ var soma = d1 + c1
+ if( soma >=10){
+
+   d1 = 0
+   c2 = 0
+   document.getElementById('adultos').innerHTML = 0;
+   document.getElementById('criancas').innerHTML = 0;
+   alert('O número máximo de passageiros deve ser de 9 pessoas.')
+}
+}
+function menos1() {
+  document.getElementById('criancas').innerHTML = --c1;
+  document.getElementById('c1').innerHTML =  c1 + " Criança(s)";
+  
+  if(c1 < 0) { //evita números negativos
+    c1 = 0
+    document.getElementById('criancas').innerHTML = 0;
+    document.getElementById('c1').innerHTML =  0;
+  }
+  if(c1 <= 0){
+    
+    document.getElementById('c1').style.display = 'none'
+    document.getElementById('e1').style.display = 'none'
+  }
+}
+
+document.querySelector('#data').onclick = function (){
+document.querySelector('input[type="date"]').style.backgroundColor = 'white'
+}
+
+
+
+kirin.addEventListener("change", function() {
+  // Verifica se o Checkbox 1 está marcado
+  if (kirin.checked) {
+      // Desmarca o Checkbox 2
+      kiri.checked = false;
+      document.querySelector('.pass').style.backgroundColor = 'white'
+   document.querySelector('.odin').style.display = 'none'
+  }
+});
+kiri.addEventListener("change", function() {
+  // Verifica se o Checkbox 1 está marcado
+  if (kiri.checked) {
+      // Desmarca o Checkbox 2
+      kirin.checked = false;
+      document.querySelector('.tipos').style.backgroundColor = 'white'
+   document.querySelector('.paii').style.display = 'none'
+  }
+});
+
+
+function click(){
+var pontos = document.getElementById("pontos")
+var mais = document.getElementById("mais")
+var button = document.getElementById('leiamais')
+
+if(mais.style.display === none ){
+  mais.style.display = 'block'
+ 
+}}
+
+
 const txt = document.querySelector('.textin')
 const nomedificil = txt.clientHeight
 const overflow = document.querySelector('.overflow')
@@ -143,161 +340,3 @@ document.addEventListener('click', function (event){
 document.getElementById('meuinpu').onclick = function(){
     novoinpute.style.backgroundColor = '#bab490';
 }
-
-
-
-
-
-
-const input = document.getElementById('ida');
-
-// Adiciona um evento de clique ao documento
-document.addEventListener('click', function (event) {
-  // Verifica se o alvo do clique não é o input
-  if (event.target !== input) {
-    // Muda a cor de fundo do input
-    input.style.backgroundColor = 'white';
-  }
-});
- 
-input.onclick = function(){
-    input.style.backgroundColor = '#bab490';
-}
-
-const novoinput = document.getElementById('volta');
-document.addEventListener('click', function (event){
-    if(event.target !== novoinput){
-        novoinput.style.backgroundColor = 'white';
-    }
-});
-document.getElementById('volta').onclick = function(){
-    novoinput.style.backgroundColor = '#bab490';
-}
-
-
-
-
-
- var kirin = document.querySelector('#korra')
- document.querySelector('.tipos').onclick = function (){
-  if (kirin.checked){
-    document.querySelector('.tipos').style.backgroundColor = '#bab490'
-    document.querySelector('.paii').style.display = 'block'
-  }else{
-    document.querySelector('.tipos').style.backgroundColor = 'white'
-    document.querySelector('.paii').style.display = 'none'
-  }
-  
- }
-
- 
-
-
-var kiri = document.querySelector('#check-pass')
-document.querySelector('.pass').onclick = function (){
- if (kiri.checked){
-   document.querySelector('.pass').style.backgroundColor = '#bab490'
-   document.querySelector('.odin').style.display = 'block'
-  
- }else{
-   document.querySelector('.pass').style.backgroundColor = 'white'
-   document.querySelector('.odin').style.display = 'none'
- }
-}
-
-
-
-var c1 = 0;
-var d1 = 0;
-var adulto = document.getElementById('adultos')
-function mais() {
-  document.getElementById('adultos').innerHTML = ++d1;
- if(d1 >= 10){
-  document.getElementById('adultos').innerHTML = 0;
-  d1 = 0
-  alert("O número máximo de passageiros deve ser de 9 pessoas.")
-  
- }
-  var soma = d1 + c1
-  if( soma >=10){
-    alert('O número máximo de passageiros deve ser de 9 pessoas.')
-    document.getElementById('adultos').innerHTML = 0;
-  d1 = 0
-    c1 = 0
-    document.getElementById('criancas').innerHTML = 0;
-  }
-}
-function menos() {
-  document.getElementById('adultos').innerHTML = --d1;
-  var atual = document.getElementById("adultos").value;
-  if(d1 < 0) { //evita números negativos
-    d1 = 0
-    document.getElementById('adultos').innerHTML = 0;
-  }
-}
-
-var crianca = document.getElementById('criancas')
-function mais1() {
-  document.getElementById('criancas').innerHTML = ++c1;
- if(c1 >= 10){
-  document.getElementById('criancas').innerHTML = 0;
-  c1 = 0
-  alert("O número máximo de passageiros deve ser de 9 pessoas.")
-  
-  
- }
- var soma = d1 + c1
- if( soma >=10){
-   alert('O número máximo de passageiros deve ser de 9 pessoas.')
-   document.getElementById('adultos').innerHTML = 0;
-   d1 = 0
-   c2 = 0
-   document.getElementById('criancas').innerHTML = 0;
-}
-}
-function menos1() {
-  document.getElementById('criancas').innerHTML = --c1;
- 
-  if(c1 < 0) { //evita números negativos
-    c1 = 0
-    document.getElementById('criancas').innerHTML = 0;
-  }
-}
-
-document.querySelector('#data').onclick = function (){
-document.querySelector('input[type="date"]').style.backgroundColor = 'white'
-}
-
-
-
-kirin.addEventListener("change", function() {
-  // Verifica se o Checkbox 1 está marcado
-  if (kirin.checked) {
-      // Desmarca o Checkbox 2
-      kiri.checked = false;
-      document.querySelector('.pass').style.backgroundColor = 'white'
-   document.querySelector('.odin').style.display = 'none'
-  }
-});
-kiri.addEventListener("change", function() {
-  // Verifica se o Checkbox 1 está marcado
-  if (kiri.checked) {
-      // Desmarca o Checkbox 2
-      kirin.checked = false;
-      document.querySelector('.tipos').style.backgroundColor = 'white'
-   document.querySelector('.paii').style.display = 'none'
-  }
-});
-
-
-function click(){
-var pontos = document.getElementById("pontos")
-var mais = document.getElementById("mais")
-var button = document.getElementById('leiamais')
-
-if(mais.style.display === none ){
-  mais.style.display = 'block'
- 
-}}
-
-
